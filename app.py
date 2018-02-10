abe0d821aedbd4c088451d14be9a281c2f8332b1
Chirayu Kapoor <32087475+C-Society@users.noreply.github.com>
2018-02-11 05:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def processRequest(req):
     Day = 3
     if Day < 5:
         # Convert time in 12 hour format
-        if now.hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 4, 3, 5]:
+        if now.hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 4, 3]:
             if now.hour > 12:
                 time = now.hour % 12
             # The CSV file
