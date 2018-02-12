3337e89fa1734f3f199b3a78bee5f62c8dfbea56
Chirayu Kapoor <32087475+C-Society@users.noreply.github.com>
2018-02-12 09:20:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def processRequest(req):
                     time = now.hour % 12
 
                 df1 = df.loc[df['Day'] == Day]
-                data1 = str(time)
-                data = data1 + " free"
+
+                data = str(time)
                 res = makeWebhookResult3(data)
                 return res
                 df2 = df1.loc[:, name]
